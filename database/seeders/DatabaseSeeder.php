cec01f16db2f376665a2719e2dfe298e2a3d779b
Umar Habib <mumarhabibrb102@gmail.com>
2025-11-04 11:37:32 +0500

    seeder issue

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,7 +13,7 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // \App\Models\User::factory(10)->create();
-$this->call([Adminseeder::class,]);
+            $this->call([AdminSeeder::class,]);
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
